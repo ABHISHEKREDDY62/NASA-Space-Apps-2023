c3e49bb41457e60479eaca7e54e55f2bd856965b
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2023-10-07 13:07:25 -0400

    done

--- execute.py
+++ execute.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, session, request, redirect, url_for,flash
+from sentence_transformers import SentenceTransformer, util
+import pandas as pd
+
+
 #from search import *
 #from get_pdf_details import *
 #from word_cloud import *
@@ -13,6 +17,19 @@ def search_index():
 def search():
     q = request.form["search"]
     print(q)
+    l=[]
+    data=pd.read_csv("senetence_similarity.csv")
+    # model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    # data["Summary"]=[str(i) for i in data["Summary"].values]
+    # for i in data["Summary"].values:
+    #     embedding_1= model.encode(q, convert_to_tensor=True)
+    #     embedding_2 = model.encode(i, convert_to_tensor=True)
+    #     l.append(util.pytorch_cos_sim(embedding_1, embedding_2))
+    # data["similarity"]=l
+    # data=data.sort_values(by="similarity",ascending=False)
+    # data.to_csv("senetence_similarity.csv")
+    document_id=data["Document ID"].values[:10]
+    print(data["similarity"].values)
     # results = search_on_keyword(q)
     # print(results)
     # pdf_details = fetch_pdf_details(results)
@@ -21,13 +38,19 @@ def search():
     # if request.method == 'POST':
     #     return render_template("embed.html", query=q, results=pdf_details["title"], 
     #     cats = pdf_details["cats"], ids = pdf_details["ids"])
+    return render_template("embed_after.html",query=q,document_id=document_id)
 
 @app.route("/pdf_details/<pdf_id>", methods=["POST", "GET"])
 def after_search(pdf_id):
-    pdf_details = fetch_pdf_details([pdf_id])
-    generate_wordcloud(pdf_id)
-    cleaned_data(pdf_id)
-    return render_template("after_search.html", details = pdf_details)
+    data=pd.read_csv("final_training_data.csv")
+    description=data[data["Document ID"]==int(pdf_id)]["Description"].values[0]
+    print(description)
+    summary=data[data["Document ID"]==int(pdf_id)]["Summary"].values[0]
+    print("this summary",summary)
+    print(data.columns)
+    #generate_wordcloud(pdf_id)
+    #cleaned_data(pdf_id)
+    return render_template("after_search.html", details = description,ID=pdf_id,summary=summary)
 
 @app.route("/embed")
 def embed():
